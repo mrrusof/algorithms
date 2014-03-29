96ca99f39a1d5ca9d914827e34bdecdbc48416c6
Ruslan Ledesma Garza <ruslanledesmagarza@gmail.com>
2014-03-29 18:24:59 +0100

    Minimum spanning trees in ocaml

---
+++ minimum-spanning-trees/main.ml
@@ -0,0 +1,52 @@
+module IntSet = Set.Make(struct type t = int let compare = compare end)
+
+let g = 
+  [(1,2);
+   (2,3);
+   (3,1);
+   (3,4);
+   (1,4);
+   (4,5);
+   (1,5);
+   (2,5);
+   (2,10);
+   (5,6);
+   (6,7);
+   (4,6);
+   (4,7);
+   (6,8);
+   (8,9);
+   (5,8);
+   (8,10);
+   (9,10);
+   (7,9);
+   (7,8);
+   (5,10)];;
+let t =
+  [(1,2);
+   (1,3);
+   (3,4);
+   (4,5);
+   (5,6);
+   (5,10);
+   (6,7);
+   (6,8);
+   (8,9)];;
+
+let rec path a b g =
+  List.mem (a,b) g ||
+  List.mem (b,a) g ||
+  List.exists (fun (a', c) -> a'=a && path c b (List.filter ((<>) (a, c)) g)) g ||
+  List.exists (fun (c, a') -> a'=a && path c b (List.filter ((<>) (c, a)) g)) g
+let vertices g =
+  let rec vs = function
+    | (x,y) :: g -> IntSet.add y (IntSet.add x (vs g))
+    | [] -> IntSet.empty
+  in IntSet.elements (vs g)
+let tree g = not (List.exists (fun n -> 
+                                Printf.printf "searching for simple cycle for %n..." n;
+                                (path n n g && (Printf.printf "yes\n"; true)) || (Printf.printf "no\n"; false))
+                              (vertices g))
+let mst t g = tree t && vertices g = vertices t 
+    
+let _ = mst t g
