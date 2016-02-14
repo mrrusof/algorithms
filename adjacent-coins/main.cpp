51c5adbb35d908bc15b1be6d15c503d27f12b4e3
Ruslan Ledesma-Garza <ruslanledesmagarza@gmail.com>
2016-02-14 00:21:08 -0600

    Revisit adjacent coins.

--- adjacent-coins/main.cpp
+++ adjacent-coins/main.cpp
@@ -1,4 +1,5 @@
 /*
+# Problem
 
 Consider N coins aligned in a row. Each coin is showing either heads
 or tails. The adjacency of these coins is the number of adjacent pairs
@@ -64,66 +65,179 @@ Complexity:
 expected worst-case time complexity is O(N);
 expected worst-case space complexity is O(1), beyond input storage
 (not counting the storage required for input arguments). 
- */
+*/
 
-
-/*
-Example: Any coin will do
-  A[0] = 0  
-  A[1] = 0  
-  A[2] = 0
-  A[3] = 0 
-  A[4] = 0
-  
-Example: Coin 2 is the one
-  A[0] = 0  
-  A[1] = 0
-  A[2] = 1 <-
-  A[3] = 0 
-  A[4] = 0
-
-  A[0] = 0
-  A[1] = 0
-  A[2] = 1
-  A[3] = 1
-  A[4] = 1
-
-P: flip a face next to different face
-Q: flip a face between different faces
-  
-  A[0] = 0
-  A[1] = 0 P
-  A[2] = 1 Q <
-  A[4] = 0
-
-  A[0] = 0
-  A[1] = 0 P
-  A[2] = 1 P
-
-P -> Same number of pairs
-Q -> 2 more pairs
-  
- */
-
-
-  
-// you can also use includes, for example:
 #include <vector>
 
 using namespace std;
-  
-int solution(const vector<int> &A) {
-  if (A.size() < 2) return 0;
-  int pairs = 0;
-  int condition = 0; /* 0 -> Neither P nor Q, 1 -> \exists C : P(C), 2 -> \exists C : Q(C)  */
-  for(int i = 1; i < A.size(); i++) {
-    if (A[i-1] == A[i]) pairs++;
-    if (A[i-1] != A[i]) {
-      if (condition == 0) condition = 1;
-      else if (i+1 < A.size() && A[i] != A[i+1]) condition = 2;
-    }
+
+/*
+# Solution
+
+Given a sequence of coins, the adjacency of the sequence either
+changes or remains the same when we flip a coin because we either
+gain pairs, lose pairs, or keep the same amount of pairs.  Consider
+the following sequence and the delta adjacency when we flip each
+coin.
+```
+Index  | Coin | Delta adjacency
+-------------------------------
+0      | 1    | -1
+1      | 1    |  0
+2      | 0    |  2
+3      | 1    |  2
+4      | 0    |  0
+5      | 0    |  0
+6      | 1    |  0
+7      | 1    | -1
+```
+The adjacency for the sequence is 3.  If we flip the first or last
+coin, we lose one pair and the adjacency becomes 2.  If we flip
+coin 2 or 3, we gained two pairs and the adjacency becomes 5.
+Flipping one of the other coins destroys a pair and creates another
+pair, so the adjacency does not change if we flip any of them.
+
+When we can gain coins, we choose to flip a coin that causes the
+biggest gain.  When we cannot gain coins, we choose to flip a coin
+that causes the smallest loss. That way we get the maximum number of
+pairs possible.  For the sequence, we flip coin 2, which gives the
+maximum possible adjacency 5.  Coin 3 is an alternative.
+
+# Implementation
+
+When the given sequence consists of one coin we return 0.  For this
+case, 0 is always the right answer because adjacency is 0 and remains 0
+after flipping the coin.
+
+When the sequence has more coins, we compute the adjacency of the
+sequence, compute the maximum delta adjacency, and return the sum
+of these two values.
+
+We compute the adjacency of the sequence by counting adjacent pairs.
+We count adjacent pairs by iterating the sequence and inspecting the
+current coin and the previous.  At each iteration, the current and
+previous coins either form a pair or not.  Each time the current coin
+and the previous show the same side, we count one pair.  Our
+implementation is the following.
+```
+ */
+int adjacency(const vector<int> &A) {
+  int adjacency = 0;
+  int previous = -1;
+  for(int i = 0; i < A.size(); i++) {
+    if(previous == A[i]) adjacency++;
+    previous = A[i];
   }
-  if (condition == 0) return pairs - 1;
-  else if (condition == 1) return pairs;
-  else if (condition == 2) return pairs+2;
+  return adjacency;
+}
+/*
+```
+
+We compute the maximum delta adjacency by computing the delta
+adjacency for each coin and keeping the maximum.  For the first and
+last coins, we apply the following rule and keep the maximum.
+```
+The first or last coin
+| Adjacent coin
+|/      Delta adjacency
+||     /
+||     |
+vv     v
+
+00 -> -1
+01 ->  1
+10 ->  1
+11 -> -1
+```
+When there are coins in the middle, we apply the following rule to
+each coin and update the maximum delta.
+```
+Coin to the left
+| A coin in the middle
+|/ Coin to the right
+||/      Delta adjacency
+|||     /
+|||     |
+vvv     v
+
+000 -> -2
+001 ->  0
+010 ->  2
+011 ->  0
+100 ->  0
+101 ->  2
+110 ->  0
+111 -> -2
+```
+Our implementation is the following.
+```
+*/
+int max(int x, int y) {
+  if(x < y) return y;
+  return x;
+}
+
+int maximum_delta_adjacency(const vector<int> &A) {
+  int maximum;
+  int last_position = A.size() - 1;
+  maximum = A[0] == A[1] ? -1 : 1;
+  maximum = max(A[last_position] == A[last_position-1] ? -1 : 1, maximum);
+  int middle[2][2][2] = {{{-2, 0}, {2, 0}}, {{0, 2}, {0, -2}}};
+  for(int i = 1; i < last_position; i++) {
+    maximum = max(middle[A[i-1]][A[i]][A[i+1]], maximum);
+  }
+  return maximum;
+}
+/*
+```
+
+The last touch is adding the adjacency and the maximum delta
+adjacency in function `solution` as follows.
+ */
+
+int solution(const vector<int> &A) {
+  if(A.size() == 1) return 0;
+  return adjacency(A) + maximum_delta_adjacency(A);
+}
+
+/*
+
+We illustrate usage of `solution` in the following program.
+
+ */
+
+#include <iostream>
+
+void print_vector(const vector<int> &A) {
+  cout << A[0];
+  for(int i = 1; i < A.size(); i++)
+    cout << ", " << A[i];
+  cout << "\n";
+}
+
+int main() {
+  int vA[] = {0};
+  vector<int> A(&vA[0], &vA[0]+1);
+  print_vector(A);
+  cout << "Maximum possible adjacency is " << solution(A) << ".\n";
+  int vB[] = {0, 0};
+  vector<int> B(&vB[0], &vB[0]+2);
+  print_vector(B);
+  cout << "Maximum possible adjacency is " << solution(B) << ".\n";
+  int vC[] = {0, 1};
+  vector<int> C(&vC[0], &vC[0]+2);
+  print_vector(C);
+  cout << "Maximum possible adjacency is " << solution(C) << ".\n";
+  int vD[] = {0, 1, 0, 1, 0, 0, 1, 0};
+  vector<int> D(&vD[0], &vD[0]+8);
+  print_vector(D);
+  cout << "Maximum possible adjacency is " << solution(D) << ".\n";
+  int vE[] = {1, 1, 0, 1, 0, 0, 1, 1};
+  vector<int> E(&vE[0], &vE[0]+8);
+  print_vector(E);
+  cout << "Maximum possible adjacency is " << solution(E) << ".\n";
+
+
+
+  return 0;
 }
