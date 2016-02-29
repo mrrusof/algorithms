6c8a6f7ffb74d7e06e81f14006f72b548b8fea41
Ruslan Ledesma-Garza <ruslanledesmagarza@gmail.com>
2016-02-29 00:56:22 -0600

    First attempt at jill rides again.

--- jill-rides-again/main.c
+++ jill-rides-again/main.c
@@ -156,16 +156,152 @@ The nicest part is between stops 3 and 9
  */
 
 
-void solve() {
+void solve(const int r, const int n[], const int s) {
+  int i = 0;
+  int len = s - 1;
 
+  printf(" r  |  a  |  ai |  aj | sep | sepi| sepj|  b  |  bi |  bj\n");
+
+  // Ignore negative prefix.
+  while(i<len && n[i]<0) {
+    printf("%3d |   0 |   0 |   0 |   0 |   0 |   0 |   0 |   0 |   0\n", n[i]);
+    i++;
+  }
+
+  // Route has no nice parts if we reached the end.
+  if(i == len) {
+    printf("Route %d has no nice parts\n", r);
+    return;
+  }
+
+  // Route has a nice part.
+
+  int a, ai, aj;
+  int b, bj;
+  int sep, sepi, sepj_bi;
+  a = aj = b = bj = sep = sepi = sepj_bi = 0;
+  ai = i + 1;
+  for(; i<len; i++) {
+    if(n[i] > 0) {
+      if(sep == 0) {
+	// Set or extend a.
+	a += n[i];
+	aj = i + 2;
+      } else {
+	// Set or extend b.
+	b += n[i];
+	bj = i + 2;
+      }
+    } else {
+      if(sep == 0) {
+	// Set sep.
+	sep = n[i];
+	sepi = i + 1;
+	sepj_bi = i + 2;
+      } else {
+	if(b == 0) {
+	  // Extend sep.
+	  sep += n[i];
+	  sepj_bi = i + 2;
+	} else {
+	  // Decide if we keep a -> sep -> b, just a, or just b.
+	  if(a >= -sep && b >= -sep) {
+	    // We may keep segment a -> sep -> b.
+
+	    if(a2j == sepi) {
+	      // Segments a and sep are adjacent.
+
+	      // Keep a -> sep -> b.
+	      a = a + sep + b;
+	      aj = bj;
+	    } else {
+	      // Segments a and sep are not adjacent.
+
+	      if(b > a) {
+		// Keep b
+		a = b;
+		ai = sepj_bi;
+		aj = bj;
+	      } else {
+		if(a < b) {
+		  // Keep a
+		} else {
+		  // 
+		}
+	      }
+	    }
+	  } else {
+	      if(b > a) {
+		// Keep b
+		a = b;
+		ai = sepj_bi;
+		aj = bj;
+	      } else {
+		// Keep a
+	      }	      
+	  }
+
+	  // Set sep and reset b.
+	  sep = n[i];
+	  sepi = i + 1;	  
+	  b = 0;
+	  sepj_bi = i + 2;
+	  bj = 0;
+	}
+      }
+    }
+
+    printf("%3d | %3d | %3d | %3d | %3d | %3d | %3d | %3d | %3d | %3d\n",
+	   n[i], a, ai, aj, sep, sepi, sepj_bi, b, sepj_bi, bj);
+  }
+
+    
+  // If segment b is set, decide if we keep a -> sep -> b, just a, or just b.
+  if(b > 0) {
+    if(a >= -sep && b >= -sep) {
+      // We may keep segment a -> sep -> b.
+
+      if(aj == sepi) {
+	// Segments a and sep are adjacent.
+
+	// Keep a -> sep -> b.
+	a = a + sep + b;
+	aj = bj;
+      } else {
+	// Segments a and sep are not adjacent.
+
+	if(b > a) {
+	  // Keep b
+	  a = b;
+	  ai = sepj_bi;
+	  aj = bj;
+	} else {
+	  // Keep a
+	}
+      }
+    } else {
+      if(b > a) {
+	// Keep b
+	a = b;
+	ai = sepj_bi;
+	aj = bj;
+      } else {
+	// Keep a
+      }	      
+    }
+  }
+
+  printf("end | %3d | %3d | %3d | %3d | %3d | %3d | %3d | %3d | %3d\n",
+	 a, ai, aj, sep, sepi, sepj_bi, b, sepj_bi, bj);
+
+  printf("The nicest part of route %d is between stops %d and %d\n", r, ai, aj);
 }
 
 int main() {
+  int r = 1;
   int s = 10;
   int n[] = {4, -5, 4, -3, 4, 4, -4, 4, -5};
-  for(int i=0; i<s-1; i++) {
-    printf("%2d\n", n[i]);
-  }
+  solve(r, n, s-1);
 
   return 0;
 }
