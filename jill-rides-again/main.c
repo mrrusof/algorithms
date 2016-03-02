a7845daa2982c8e615f0340a0d70d52bd2cf4bf7
Ruslan Ledesma-Garza <ruslanledesmagarza@gmail.com>
2016-03-02 10:09:19 -0600

    wip

--- jill-rides-again/main.c
+++ jill-rides-again/main.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <stdio.h>
 
 /*
@@ -155,7 +156,7 @@ The nicest part is between stops 3 and 9
 
  */
 
-
+/*
 void solve(const int r, const int n[], const int s) {
   int i = 0;
   int len = s - 1;
@@ -296,12 +297,120 @@ void solve(const int r, const int n[], const int s) {
 
   printf("The nicest part of route %d is between stops %d and %d\n", r, ai, aj);
 }
+*/
+
+typedef struct segment {
+  int value;
+  int i;
+  int j;
+  struct segment* prev;
+  struct segment* next;
+} segment;
+
+typedef struct workitem {
+  struct segment* item;
+  struct workitem* next;
+} workitem;
+
+void solve2(const int r, const int n[], const int s) {
+  int i = 0;
+  int len = s - 1;
+  struct segment* first;
+  struct segment* curr;
+  struct segment* prev;
+  struct workitem* work;
+  struct workitem* prev_work;
+
+  // Ignore negative prefix.
+  while(i<len && n[i]<0) i++;
+  
+  // Route has no nice parts if we reached the end.
+  if(i == len) {
+    printf("Route %d has no nice parts\n", r);
+    return;
+  }
+
+  // Preprocess the route.
+  first = curr = (segment *)malloc(sizeof(segment));
+  curr->value = n[i];
+  curr->i = i + 1;
+  curr->j = i + 2;
+  curr->prev = NULL;
+  curr->next = NULL;
+  i++;
+  while(i<len) {
+    if((curr->value > 0 && n[i] > 0) || (curr->value < 0 && n[i] < 0)) {
+      curr->value += n[i];
+      curr->j++;
+    } else {
+      prev = curr;
+      curr = (segment *)malloc(sizeof(segment));
+      prev->next = curr;
+      curr->value = n[i];
+      curr->i = i + 1;
+      curr->j = i + 2;
+      curr->prev = curr;
+      curr->next = NULL;
+    }
+    i++;
+  }
+
+  // Cut a negative tail.
+  if(curr->value < 0) {
+    prev = curr->prev;
+    prev->next = NULL;
+    free(curr);
+  }
+
+  // Populate initial worklist.
+  curr = first;
+  work = NULL;
+  while(curr != NULL) {
+    if(curr->value < 0) {
+      prev_work = work;
+      work = (workitem *)malloc(sizeof(workitem));
+      work->item = curr;
+      work->next = prev_work;
+    }
+    curr = curr->next;
+  }
+
+  // Process worklist.
+  while(work != NULL) {
+    curr = work->item;
+    prev_work = work;
+    work = work->next;
+    free(prev_work);
+    if(curr->prev->value >= -curr->value && curr->next->value >= -curr->value) {
+      // Join segments.
+      prev = curr;
+      curr = curr->prev;
+      curr->value += prev->value + prev->next->value;
+      curr->j = prev->next->j;
+      curr->next = prev->next->next;
+      prev->next->prev = curr;
+      free(prev->next);
+      free(prev);
+      // Add neighbour segments.
+      
+    }
+  }
+
+  // Free the route.
+  curr = first;
+  while(curr != NULL) {
+    printf("%2d\n", curr->value);
+    prev = curr;
+    curr = curr->next;
+    free(prev);
+  }
+}
 
 int main() {
   int r = 1;
   int s = 10;
   int n[] = {4, -5, 4, -3, 4, 4, -4, 4, -5};
-  solve(r, n, s-1);
+  solve2(r, n, s);
 
   return 0;
 }
