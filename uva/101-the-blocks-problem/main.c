2d45d4fefd8d5023ee827e607ef0412871ea98b4
Ruslan Ledesma-Garza <ruslanledesmagarza@gmail.com>
2016-03-20 15:40:51 -0600

    Revisit solution for the blocks problem.

--- uva/101-the-blocks-problem/main.c
+++ uva/101-the-blocks-problem/main.c
@@ -1,132 +1,120 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define Si(a) scanf("%d", &a)
+#define Ss(a) scanf("%s", a)
+
 #define MAX 25
 
 typedef struct block {
   struct block *above;
   struct block *below;
-  int stack;
-  int v;
+  int table_loc;
+  int value;
 } block;
 
-struct block *table[MAX];
+block *table[MAX];
 
-struct block *top(struct block *a) {
-  while(a->above != NULL) a = a->above;
-  return a;
-}
-
-void set_stack(struct block *a, int stack) {
-  while(a != NULL) {
-    a->stack = stack;
-    a = a->above;
-  }
-}
-
-void over(struct block *a, struct block *b) {
-  struct block *top_b = top(b);
-  if(a->below != NULL) a->below->above = NULL;
-  else table[a->v] = NULL;
-  top_b->above = a;
-  a->below = top_b;
-}
-
-void put_back(struct block *a) {
-  struct block *b;
+void put_back(block *a) {
+  block *b;
   while(a->above != NULL) {
     b = a->above;
     a->above = NULL;
     b->below = NULL;
-    b->stack = b->v;
-    table[b->v] = b;
+    b->table_loc = b->value;
+    table[b->value] = b;
     a = b;
   }
 }
 
-void onto(struct block *a, struct block *b) {
-  put_back(b);
-  over(a, b);
+block *top(struct block *a) {
+  while(a->above != NULL) a = a->above;
+  return a;
+}
+
+void stack(block *a, block *b) {
+  block *top_b = top(b);
+  block *a_below = a->below;
+  int table_loc = b->table_loc;
+
+  /* Remove stack a from its current position. */
+  if(a_below != NULL) a_below->above = NULL;
+  else table[a->value] = NULL;
+
+  /* Stack stack a onto the top of stack b. */
+  top_b->above = a;
+  a->below = top_b;
+
+  /* Update stack reference for block a and blocks above a. */
+  while(a != NULL) {
+    a->table_loc = table_loc;
+    a = a->above;
+  }
 }
 
 void print_table(int n) {
-  struct block *a;
+  block *a;
   int i;
   for(i = 0; i < n; i++) {
     printf("%d:", i);
     for(a = table[i]; a != NULL; a = a->above)  {
-      printf(" %d", a->v);
+      printf(" %d", a->value);
     }
     printf("\n");
   }
 }
 
-/* #define DEBUG 1 */
-#define Si(a) scanf("%d", &a)
-#define Ss(a) scanf("%s", a)
-
 int main() {
   int n, i, v;
-  struct block *blocks[MAX];
-  struct block *a, *b;
+  char verb[4], prep[4];
+  block *blocks[MAX];
+  block *a, *b;
 
   for(i = 0; i < MAX; i++)
     table[i] = blocks[i] = NULL;
 
+  /* Read number of blocks `n`. */
   Si(n);
 
+  /* Initialize world with `n` blocks. */
   for(i = 0; i < n; i++) {
-    table[i] = blocks[i] = (block *)malloc(sizeof(block));
+    table[i] = blocks[i] = (block *) malloc(sizeof(block));
     blocks[i]->above = NULL;
     blocks[i]->below = NULL;
-    blocks[i]->stack = i;
-    blocks[i]->v = i;
+    blocks[i]->table_loc = i;
+    blocks[i]->value = i;
   }
 
-  char verb[4];
-  char prep[4];
-  while(1 == 1) {
+  while(1) {
+
+    /* Read the verb. */
     Ss(verb);
 
+    /* Exit when verb is quit. */
     if(verb[0] == 'q') break;
 
+    /* Read the object. */
     Si(v);
     a = blocks[v];
+    
+    /* Read the preposition. */
     Ss(prep);
+
+    /* Read the complement. */
     Si(v);
     b = blocks[v];
 
-    if(a->stack == b->stack) continue;
+    /* Ignore action if it is illegal. */
+    if(a->table_loc == b->table_loc) continue;
 
-    if(verb[0] == 'm') {
-#ifdef DEBUG
-      printf("move %d ", a->v);
-#endif
-      put_back(a);
-    }
-#ifdef DEBUG
-    else if(verb[0] == 'p') {
-      printf("pile %d ", b->v);
-    }
-#endif
+    /* When verb is `move`, put back that are above the object `a`. */
+    if(verb[0] == 'm') put_back(a);
 
-    set_stack(a, b->stack);
+    /* When preposition is `onto`, put back the blocks that are above the complement `b`. */
+    if(prep[1] == 'n') put_back(b);
 
-    if(prep[1] == 'n') {
-#ifdef DEBUG
-      printf("onto %d\n", b->v);
-#endif
-      onto(a, b);
-    } else {
-#ifdef DEBUG
-      printf("over %d\n", b->v);
-#endif
-      over(a, b);
-    }
-
-#ifdef DEBUG
-    print_table(n);
-#endif
+    /* Pile object `a` over complement `b`. */
+    stack(a, b);
   }
 
   print_table(n);
