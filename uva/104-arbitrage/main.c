bb021e40ad5c1c5431d90d41ec2df99d406e7adc
Ruslan Ledesma-Garza <ruslanledesmagarza@gmail.com>
2016-05-10 09:43:28 -0500

    Alternative solution to 104.

--- uva/104-arbitrage/main.c
+++ uva/104-arbitrage/main.c
@@ -16,32 +16,55 @@ int node_count;
 int paths[MAX_DIM][MAX_DIM][MAX_DIM + 1];
 float rates[MAX_DIM][MAX_DIM][MAX_DIM + 1];
 
-/* Any of the most profitable paths of length `l` from `a` to `b`. */
-void f(int a, int b, int l) {
+/* Any of the most profitable paths of length `l` from `a` to `t` that we have not considered. */
+void f(int a, int r, int l) {
+#if DEBUG
   int i;
+  for(i = 0; i < node_count - l; i++)
+    printf(" ");
+  printf("f(%d, %d, %d) =\n", a, r, l);
+#endif
+  int b;
   float rate;
-  if(paths[a][b][l] != FRESH) return;
-  if(l == 1) {
-    if(edge[a][b] == NOTHING) {
-      paths[a][b][l] = NOTHING;
-      rates[a][b][l] = NOTHING;
-    } else {
-      paths[a][b][l] = b;
-      rates[a][b][l] = edge[a][b];
-    }
+  if(paths[a][r][l] != FRESH) {
+#if DEBUG
+    for(i = 0; i < node_count - l; i++)
+      printf(" ");
+    printf("%f\n", rates[a][r][l]);
+#endif
     return;
   }
-  rates[a][b][l] = NOTHING;
-  paths[a][b][l] = NOTHING;
-  for(i = 0; i < node_count; i++) {
-    if(a == i) continue;
-    f(i, b, l - 1);
-    rate = edge[a][i] * rates[i][b][l - 1];
-    if(rates[a][b][l] < rate) {
-      rates[a][b][l] = rate;
-      paths[a][b][l] = i;
+  if(l == 1) {
+    if(edge[a][r] == NOTHING) {
+      paths[a][r][1] = NOTHING;
+      rates[a][r][1] = NOTHING;
+    } else {
+      paths[a][r][1] = r;
+      rates[a][r][1] = edge[a][r];
+    }
+#if DEBUG
+  for(i = 0; i < node_count - l; i++)
+    printf(" ");
+  printf("%f (new)\n", rates[a][r][l]);
+#endif
+    return;
+  }
+  rates[a][r][l] = NOTHING;
+  paths[a][r][l] = NOTHING;
+  for(b = r; b < node_count; b++) {
+    if(a == b) continue;
+    f(b, r, l - 1);
+    rate = edge[a][b] * rates[b][r][l - 1];
+    if(rates[a][r][l] < rate) {
+      rates[a][r][l] = rate;
+      paths[a][r][l] = b;
     }
   }
+#if DEBUG
+  for(i = 0; i < node_count - l; i++)
+    printf(" ");
+  printf("%f (new)\n", rates[a][r][l]);
+#endif
 }
 
 void print_edges(float edge[MAX_DIM][MAX_DIM], int n) {
@@ -56,7 +79,7 @@ void print_edges(float edge[MAX_DIM][MAX_DIM], int n) {
 
 int main() {
 
-  int i, j, k;
+  int i, j, l;
 
   while(Si(node_count) != EOF) {
 
@@ -77,20 +100,29 @@ int main() {
     /* Reset paths and rates. */
     for(i = 0; i < node_count; i++)
       for(j = 0; j < node_count; j++)
-	for(k = 0; k <= node_count; k++) {
-	  paths[i][j][k] = FRESH;
-	  rates[i][j][k] = FRESH;
+	for(l = 0; l <= node_count; l++) {
+	  paths[i][j][l] = FRESH;
+	  rates[i][j][l] = FRESH;
 	}
 
     /* Consider cycles in ascending length. */
     int stop = 0;
-    for(i = 2; i <= node_count && stop == 0; i++) {
-      for(j = 0; j < node_count && stop == 0; j++) {
-	f(j, j, i);
-	if(rates[j][j][i] >= MIN_PROFIT)
+    int r;
+    for(l = 2; l <= node_count && stop == 0; l++) {
+#if DEBUG
+      printf("l = %d\n", l);
+#endif
+      for(r = 0; r < node_count - l + 1 && stop == 0; r++) {
+#if DEBUG
+	printf("r = %d\n", r);
+	printf("node_count = %d\n", node_count);
+	printf("node_count - l = %d\n", node_count - l);
+	printf("node_count - l + 1 = %d\n", node_count - l + 1);
+#endif
+	f(r, r, l);
+	if(rates[r][r][l] >= MIN_PROFIT)
 	  stop = 1;
 #if DEBUG
-	printf("f(%d, %d, %d)\n", j, j, i);
 	printf("stop = %d\n", stop);
 #endif
       }
@@ -98,14 +130,14 @@ int main() {
 
     /* Print answer. */
     if(stop) {
-      int pivot = j - 1;
-      int len = i - 1;
-      int curr = pivot;
+      int first = r - 1;
+      int len = l - 1;
+      int curr = first;
       while(len > 0) {
 	printf("%d ", curr + 1);
-	curr = paths[curr][pivot][len--];
+	curr = paths[curr][first][len--];
       }
-      printf("%d\n", pivot + 1);
+      printf("%d\n", first + 1);
     } else {
       printf("no arbitrage sequence exists\n");
     }
