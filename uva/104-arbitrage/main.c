1e379a3b4e3bd5197b9f6a29358a5184a9401b21
Ruslan Ledesma Garza <ruslanledesmagarza@gmail.com>
2016-05-18 13:22:01 -0500

    Algorithm S for 104.

--- uva/104-arbitrage/main.c
+++ uva/104-arbitrage/main.c
@@ -6,7 +6,7 @@
 #define Si(n) scanf("%d", &n)
 #define Sf(n) scanf("%f", &n)
 
-void print_path(int s, int t, int l, int succ[V+1][V][V]) {
+void print_path(int s, int t, int l, int succ[V][V][V]) {
   if(l == 0)
     printf("%d\n", s + 1);
   else {
@@ -22,32 +22,27 @@ void set_to_zero(int n, float m[V][V]) {
       m[i][j] = 0;
 }
 
-void R(int n, float rate[V][V]) {
-  float benefit[V+1][V][V];
-  int succ[V+1][V][V];
+void S(int n, float rate[V][V]) {
+  float benefit[V][V][V];
+  int succ[V][V][V];
   int l, i, j, k;
-  set_to_zero(n, benefit[0]);
   for(i = 0; i < n; i++)
-    benefit[0][i][i] = 1;
-  set_to_zero(n, benefit[1]);
+    for(j = 0; j < n; j++)
+      benefit[1][i][j] = rate[i][j];
   for(l = 2; l <= n; l++) {
     set_to_zero(n, benefit[l]);
     for(i = 0; i < n; i++)
-      for(j = i + 1; j < n; j++) {
-	for(k = i; k < n; k++)
-	  if(benefit[l - 1][j][i] < rate[j][k] * benefit[l - 2][k][i]) {
-	    benefit[l - 1][j][i] = rate[j][k] * benefit[l - 2][k][i];
-	    succ[l - 1][j][i] = k;
+      for(j = 0; j < n; j++)
+	for(k = 0; k < n; k++) {
+	  if(benefit[l][i][j] < rate[i][k] * benefit[l - 1][k][j]) {
+	    benefit[l][i][j] = rate[i][k] * benefit[l - 1][k][j];
+	    succ[l][j][i] = k;
+	    if(i == j && benefit[l][i][j] >= MIN_PROFIT) {
+	      print_path(i, i, l, succ);
+	      return;
+	    }
 	  }
-	if(benefit[l][i][i] < rate[i][j] * benefit[l - 1][j][i]) {
-	  benefit[l][i][i] = rate[i][j] * benefit[l - 1][j][i];
-	  succ[l][i][i] = j;
 	}
-	if(benefit[l][i][i] >= MIN_PROFIT) {
-	  print_path(i, i, l, succ);
-	  return;
-	}
-      }
   }
   printf("no arbitrage sequence exists\n");
 }
@@ -66,7 +61,7 @@ int main() {
 	Sf(r);
 	rate[i][j] = r;
       }
-    R(n, rate);
+    S(n, rate);
   }
   return 0;
 }
