7bc941311e16ac5ee051fd4f34119b586edf8b92
Ruslan Ledesma Garza <ruslanledesmagarza@gmail.com>
2016-06-10 19:41:16 -0500

    Correct offset for Johnson-Trotter.

--- permutations/johnson-trotter/main.c
+++ permutations/johnson-trotter/main.c
@@ -1,88 +1,79 @@
 #include <stdio.h>
 
-int swap_count;
+#define Ss(s) scanf("%s", s)
+
+#define MAX_LEN 1000
+
+int count;
 
 void print_swap(int i, int n) {
-  int k;
-  printf("%3d ", ++swap_count);
-  for(k = 0; k < n-1; k++) {
-    if(k == i)
-      printf("|<-->");
+  int j;
+  printf("%05d ", count++);
+  for(j = 0; j < n - 1; j++)
+    if(i == j)
+      printf("|<--->");
     else
-      printf("|    ");
-  }
+      printf("|     ");
   printf("|");
 }
 
-void adjacent_swap(char *a) {
-  char t = *a;
-  *a = *(a + 1);
-  *(a + 1) = t;
+void swap_w_next(char *s, int i) {
+  char t = s[i];
+  s[i] = s[i + 1];
+  s[i + 1] = t;
 }
 
-/*
-  1                     ABCD
-  2 .<-->.    .    .    BACD    L = 4   a = 0   b = 1
-  3 .    .<-->.    .    BCAD    L = 4   a = 1   b = 2
-  4 .    .    .<-->.    BCDA    L = 4   a = 2   b = 3
-  5 .<-->.    .    .    CBDA    L = 3   a = 0   b = 1
-  6 .    .    .<-->.    CBAD    L = 4   a = 2   b = 3
-  7 .    .<-->.    .    CABD    L = 4   a = 1   b = 2
-  8 .<-->.    .    .    ACBD    L = 4   a = 0   b = 1
-  9 .    .    .<-->.    ACDB    L = 3   a = 2   b = 3
- 10 .<-->.    .    .    CADB    L = 4   a = 0   b = 1
- 11 .    .<-->.    .    CDAB    L = 4   a = 1   b = 2
- 12 .    .    .<-->.    CDBA    L = 4   a = 2   b = 3
- 13 .<-->.    .    .    DCBA    L = 2   a = 0   b = 1
- 14 .    .    .<-->.    DCAB    L = 4   a = 2   b = 3
- 15 .    .<-->.    .    DACB    L = 4   a = 1   b = 2
- 16 .<-->.    .    .    ADCB    L = 4   a = 0   b = 1
- 17 .    .    .<-->.    ADBC    L = 3   a = 2   b = 3
- 18 .<-->.    .    .    DABC    L = 4   a = 0   b = 1
- 19 .    .<-->.    .    DBAC    L = 4   a = 1   b = 2
- 20 .    .    .<-->.    DBCA    L = 4   a = 2   b = 3
- 21 .<-->.    .    .    BDCA    L = 3   a = 0   b = 1
- 22 .    .    .<-->.    BDAC    L = 4   a = 2   b = 3
- 23 .    .<-->.    .    BADC    L = 4   a = 1   b = 2
- 24 .<-->.    .    .    ABDC    L = 4   a = 0   b = 1
-*/
+void jt(char *s, int n, int l, int *p, int *o);
 
-void johnson_trotter(char *s, int l, int n, int *d) {
-  int i, a, offset = 0;
-  for(i = 0; i < l - 1; i++) {
-    if(l < n) {
-      johnson_trotter(s, l + 1, n, d);
-      if(d[n - 1] == -1)
-	offset = 0;
-      else
-	offset = 1;
-    }
-    if(d[l - 1] == 1)
-      a = offset + i;
-    else
-      a = offset + l - 2 - i;
-    adjacent_swap(s + a);
-    print_swap(a, n);
-    printf(" %s\tL = %d\ta = %d\tb = %d\n", s, l, a, a + 1);
-  }
+void step(char *s, int n, int l, int *p, int *o,int i) {
   if(l < n)
-    johnson_trotter(s, l + 1, n, d);
-  d[l - 1] *= -1;
+    jt(s, n, l + 1, p, o);
+  swap_w_next(s, o[l - 1] + i);
+  print_swap(o[l - 1] + i, n);
+  printf(" %s\tL = %5d\ta = %5d\tb = %5d\n", s, l, o[l - 1] + i, o[l - 1] + i + 1);
 }
 
-#define N 6
+void jt(char *s, int n, int l, int *p, int *o) {
+  int i;
+  if(p[l - 1])
+    for(i = 0; i < l - 1; i++)
+      step(s, n, l, p, o, i);
+  else
+    for(i = l - 2; 0 <= i; i--)
+      step(s, n, l, p, o, i);
+  if(l < n)
+    jt(s, n, l + 1, p, o);
+  if(l == n)
+    o[n - 1] = 0;
+  if(p[l - 1])
+    o[l - 2] = o[l - 1];
+  else
+    o[l - 2] = o[l - 1] + 1;
+  p[l - 1] = !p[l - 1];
+}
+
+void johnson_trotter(char *s, int len) {
+  int i, p[len], o[len];
+  if(len < 2)
+    printf("The only permutation is %s", s);
+  else {
+    count = 1;
+    print_swap(-1, len);
+    printf(" %s\n", s);
+    for(i = 0; i < len; i++) {
+      p[i] = 1;
+      o[i] = 0;
+    }
+    jt(s, len, 2, p, o);
+  }
+}
 
 int main() {
-  int i;
-  char s[N+1] = "ABCDEF";
-  int d[N];
-  for(i = 0; i < N; i++)
-    d[i] = 1;
-  printf("  1 ");
-  for(i = 0; i < N-1; i++)
-    printf("|    ");
-  printf("| %s\n", s);
-  swap_count = 1;
-  johnson_trotter(s, 2, N, d);
+  int len;
+  char s[MAX_LEN];
+  Ss(s);
+  for(len = 0; s[len] != '\0'; len++);
+  printf("\n");
+  johnson_trotter(s, len);
   return 0;
 }
