80a850ae9d79096bb5ce373a613207ef3c631314
Ruslan Ledesma Garza <ruslanledesmagarza@gmail.com>
2016-11-05 14:20:24 -0600

    Remove problem statement from Ruby sol to Pattern Matching Problem.

--- pattern-matching-problem/ruby/main.rb
+++ pattern-matching-problem/ruby/main.rb
@@ -1,58 +1,5 @@
 #!/usr/bin/env ruby
 
-=begin
-
-Input.
-A set of words and a set of patterns.  Each word and each pattern is
-given in a line by itself.  Words and patterns are separated by
-character #.  The input is terminated by EOF.
-
-Output.
-For each pattern, write in a single line the pattern, a space, and
-true if the pattern matches a word or false if it does not.  Output
-patterns in the order you read them.
-
-Example.
-
-Input.
-
-```
-coin
-coins
-does
-dork
-why
-work
-#
-coin
-coins
-c
-co
-coi
-coins
-coins.
-.coins
-does
-d
-do
-doe
-.oes
-d.es
-do.s
-doe.
-w..
-w...
-w....
-```
-
-Output.
-
-```
-
-```
-
-=end
-
 class Hash
 
   def save_word w
@@ -86,7 +33,6 @@ def main
     break if w == '#'
     words.save_word w
   end
-#  puts words
   while true
     begin
       p = readline.strip
