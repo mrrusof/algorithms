37b8a55ab225045fe15af3fc4411c0b757429c22
Ruslan Ledesma Garza <ruslanledesmagarza@gmail.com>
2016-11-28 00:00:18 -0600

    Clean previous solution.

--- substring-with-concatenation-of-all-words/ruby/main.rb
+++ substring-with-concatenation-of-all-words/ruby/main.rb
@@ -16,33 +16,23 @@ You should return the indices: [0,9].
 
 =end
 
-def reset_word_count wc, ww
-  ww.each { |w| if wc.has_key? w then wc[w] += 1 else wc[w] = 1 end }
-end
-
-def do_the_counting offset, l, s, ww
-  # puts "s = #{s}"
+def do_et offset, l, s, ww
   i = 0
   tt = []
   while i + l - 1 < s.length
     tt << s[i..i+l-1]
     i += l
   end
-  # puts "tt = #{tt}"
   o = []
   ttq = []
   wc = {}
-  reset_word_count wc, ww
+  ww.each { |w| if wc.has_key? w then wc[w] += 1 else wc[w] = 1 end }
   tt.each_with_index do |t, i|
-    # puts "1 ttq = #{ttq}"
-    # puts "1 wc = #{wc}"
     # expand
     ttq << [t, i * l + offset]
     if wc.has_key? t
       wc[t] -= 1
     end
-    # puts "2 ttq = #{ttq}"
-    # puts "2 wc = #{wc}"
     # contract
     if ttq.length > ww.length
       h, _ = ttq.delete_at 0
@@ -51,20 +41,17 @@ def do_the_counting offset, l, s, ww
         wc[h] += 1
       end
     end
-    # puts "3 ttq = #{ttq}"
-    # puts "3 wc = #{wc}"
+    # check
     if wc.all? { |k, v| v == 0 }
       o << ttq.first[1]
     end
-    # puts "o = #{o}"
   end
-  # puts "o = #{o}"
   return o
 end
 
 def swcoaw s, ww
   l = ww.first.length
-  return (0..l-1).reduce([]) { |o, i| o + do_the_counting(i, l, s[i..-1], ww) }
+  return (0..l-1).reduce([]) { |o, i| o + do_et(i, l, s[i..-1], ww) }
 end
 
 [ [ "barfoothefoobarman", ["foo", "bar"], [0,9] ],
