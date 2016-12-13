df2339f904e6a1921a252b9a1bf54a242cb6826d
Ruslan Ledesma Garza <ruslanledesmagarza@gmail.com>
2016-12-12 22:38:55 -0600

    Write less contrived solution to corner to corner matrix traversal.

--- corner-to-corner-matrix-traversal/main.rb
+++ corner-to-corner-matrix-traversal/main.rb
@@ -16,18 +16,9 @@ Example. The answer for the following matrix is 29.
 =end
 
 def traverse m
-  n = m.length
-  (0..n-1).each do |r|
-    (0..r).each do |j|
-      i = r - j
-      u = if i > 0 then m[i-1][j] else 0 end
-      l = if j > 0 then m[i][j-1] else 0 end
-      m[i][j] = [u, l].max + m[i][j]
-    end
-  end
-  (1..n-1).each do |r|
-    (n-1).downto(r).each do |j|
-      i = r + n-1 - j
+  n = m.length - 1
+  (0..n).each do |i|
+    (0..n).each do |j|
       u = if i > 0 then m[i-1][j] else 0 end
       l = if j > 0 then m[i][j-1] else 0 end
       m[i][j] = [u, l].max + m[i][j]
@@ -38,6 +29,7 @@ end
 
 [ [[[1,2,3],[4,5,6],[7,8,9]], 29],
   [[[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]], 73],
+  [[[1,2,3,4],[5,6,7,50],[9,10,11,12],[13,14,15,16]], 97],
   [[[10,-4,3],[5,2,6],[-1,7,9]], 33]
 ].each do |m, exp|
   act = traverse m
