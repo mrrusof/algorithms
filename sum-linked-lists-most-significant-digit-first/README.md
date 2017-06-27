e50fa80ad35f630e2695bd0f5d29d35bf722b60b
Ruslan Ledesma Garza <ruslanledesmagarza@gmail.com>
2017-06-26 21:16:02 -0500

    Solve "Sum Linked List Most Significant Digit First" in Ruby.

---
+++ sum-linked-lists-most-significant-digit-first/README.md
@@ -0,0 +1,17 @@
+# Sum Linked Lists
+
+You are given the linked list representation of two integers.
+Your task is to return the linked list representation of their sum.
+
+Example:
+
+**Input.**
+```
+817: -> 8 -> 1 -> 7
+ 19:      -> 1 -> 9
+```
+
+**Output.**
+```
+836: -> 8 -> 3 -> 6
+```
---
+++ sum-linked-lists-most-significant-digit-first/ruby/main.rb
@@ -0,0 +1,73 @@
+class Node
+  attr_accessor :next, :value
+
+  def initialize n, v
+    @next = n
+    @value = v
+  end
+
+  def to_s
+    out = "#{value}"
+    h = self
+    while h.next != nil
+      h = h.next
+      out += " #{h.value}"
+    end
+    return out
+  end
+end
+
+def reverse h
+  r = nil
+  while h != nil
+    n = h.next
+    h.next = r
+    r = h
+    h = n
+  end
+  return r
+end
+
+def sum l, r
+  l = reverse l
+  r = reverse r
+  carried = 0
+  h = c = Node.new nil, 0
+  while l != nil or r != nil or carried > 0
+    if l != nil
+      lv = l.value
+      l = l.next
+    else
+      lv = 0
+    end
+    if r != nil
+      rv = r.value
+      r = r.next
+    else
+      rv = 0
+    end
+    sum = carried + lv + rv
+    c.next = Node.new nil, (sum % 10)
+    c = c.next
+    carried = sum / 10
+  end
+  return reverse h.next
+end
+
+def read_linked_list
+  nn = readline.strip.split(' ').map(&:to_i)
+  return nil if nn.empty?
+  h = c = Node.new nil, nn[0]
+  nn[1..-1].each do |n|
+    c = c.next = Node.new nil, n
+  end
+  return h
+end
+
+while true
+  l = read_linked_list rescue break
+  r = read_linked_list rescue break
+  puts "l = #{l}"
+  puts "r = #{r}"
+  puts "s = #{sum l, r}"
+end
