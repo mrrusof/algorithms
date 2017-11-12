81de6d609750e8ff144d7c89e9b50b0e2ef0f517
Ruslan Ledesma Garza <ruslanledesmagarza@gmail.com>
2017-11-12 15:31:48 -0600

    Really solve "Group Frequencies".

--- group-fequencies/ruby/main.rb
+++ group-fequencies/ruby/main.rb
@@ -1,25 +1,43 @@
 #!/usr/bin/env ruby
 
+def sum_reachable g, c, v, f
+  return nil if !g[c]
+  work = [c]
+  v[c] = true
+  s = f[c]
+  while !work.empty?
+    p = work.pop
+    g[p].each do |c|
+      next if v[c]
+      work << c
+      v[c] = true
+      s += f[c]
+    end             
+  end
+  return s
+end
+
 def group_frequencies freqs, eqs
-  bb = {}
-  rf = {}
-  eqs.each do |s1, s2|
-    if !bb[s1] && !bb[s2]
-      bb[s1] = s2
-      bb[s2] = s2
-    elsif !bb[s1]
-      bb[s1] = bb[s2]
-    elsif !bb[s2]
-      bb[s2] = bb[s1]
+  rf = []
+  g = {}
+  visited = {}
+  freqs = freqs.to_h
+  eqs.each do |a, b|
+    g[a] ||= []
+    g[a] << b
+    g[b] ||= []
+    g[b] << a
+  end
+  freqs.each do |n, f|
+    next if !!visited[n]
+    s = sum_reachable g, n, visited, freqs
+    if !!s
+      rf << [n, s]
+    else
+      rf << [n, f]
     end
   end
-  freqs.each do |s, f|
-    b = bb[s]
-    b ||= s
-    rf[b] ||= 0
-    rf[b] += f
-  end
-  return rf.to_a
+  return rf
 end
 
 def main
@@ -27,22 +45,52 @@ def main
     [ [ ['a', 1],
         ['aa', 2],
         ['b', 4] ],
-      [ ['a', 'aa'] ] ],
+      [ ['a', 'aa'] ],
+      [ ['a', 3],
+        ['b', 4] ] ],
     [ [ ['a', 1],
         ['aa', 2] ],
-      [ ['b', 'bb'] ] ],
+      [ ['b', 'bb'] ],
+      [ ['a', 1],
+        ['aa', 2] ] ],
     [ [ ['a', 1],
         ['aa', 2],
         ['c', 5]],
       [ ['aa', 'a'],
-        ['aa', 'c'] ] ],
+        ['aa', 'c'] ],
+      [ ['a', 8] ] ],
     [ [ ['a', 1],
         ['aa', 2],
         ['c', 5]],
       [ ['aa', 'a'],
-        ['c', 'aa'] ] ]
-  ].each do |freqs, eqs|
-    puts "group_frequencies #{freqs}, #{eqs} = #{group_frequencies freqs, eqs}"
+        ['c', 'aa'] ],
+      [ ['a', 8] ] ],
+    [ [ ['a1', 10],
+        ['a2', 3],
+        ['a3', 9],
+        ['a4', 11],
+        ['x', 2],
+        ['b1', 3],
+        ['b2', 5],
+        ['c1', 8],
+        ['c2', 2] ],
+      [ ['a3', 'a1'],
+        ['a2', 'a4'],
+        ['a4', 'a1'],
+        ['b1', 'b2'],
+        ['c2', 'c1'] ],
+      [ ['a1', 33],
+        ['x', 2],
+        ['b1', 8],
+        ['c1', 10] ] ]
+  ].each do |freqs, eqs, exp|
+    act = group_frequencies freqs, eqs
+    print "group_frequencies #{freqs}, #{eqs} = #{act} ... "
+    if act == exp
+      puts 'PASS'
+    else
+      puts 'FAIL'
+    end
   end
 end
 
