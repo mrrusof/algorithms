6fcd40d5d21587e15fe03fab1469a51d397b4a0c
Ruslan Ledesma Garza <ruslanledesmagarza@gmail.com>
2018-05-22 23:01:24 -0500

    Rename main.java -> Main.java for "Most Recent Common Ancestor".

--- most-recent-common-ancestor/java/Main.java
+++ most-recent-common-ancestor/java/Main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.HashMap;
 
-public class main {
+public class Main {
     public static void main(String... args) {
         Scanner in = new Scanner(System.in);
         while(in.hasNextInt()) {
