39cfdeca57abc4602b578a8dc0a10751bf87fe9c
Ruslan Ledesma Garza <ruslanledesmagarza@gmail.com>
2018-06-22 23:28:06 -0500

    Watermark: solution for TBOP.

--- watermark/ruby/main.rb
+++ watermark/ruby/main.rb
@@ -6,7 +6,7 @@ def volume_of_segment h, s, e
   h[(s+1)..(e-1)].each do |n|
     volume += watermak - n
   end
-  puts "volume_of_segment #{h}, #{s}, #{e} = #{volume}"
+#  puts "volume_of_segment h, #{s}, #{e} = #{volume}"
   return volume
 end
 
@@ -26,14 +26,28 @@ def volume_increasing h
 end
 
 def volume h
+  return 0 if !!h || h.empty
   max = h.max
   maxi = h.find_index max
   first_half = h[0..maxi]
   second_half = h[maxi..-1].reverse
-  return volume_increasing(first_half) + volume_increasing(second_half)
+  vfh = volume_increasing(first_half)
+#  puts "vfh = #{vfh}"
+  vsh = volume_increasing(second_half)
+#  puts "vsh = #{vsh}"
+  return vfh + vsh
 end
 
-puts volume [0, 1, 0, 1, 0] # 1
-puts volume [0, 1, 0, 2, 0, 3, 1, 2, 1, 3] # 8
-puts volume [0, 1, 0, 2, 0, 3, 1, 2, 1, 3, 0, 0] # 8
-puts volume [0, 0, 0, 1, 0, 5, 0, 3, 0, 0, 6, 0, 0, 4, 0, 0] # 26
+while true
+  h = readline.strip.split(' ').map(&:to_i) rescue break
+  puts volume h
+end
+
+# puts volume [3, 2, 1, 0, 1, 2, 3] # 9
+# puts volume [-3, -2, -1, 0, 1, 2, 3] # 0
+# puts volume [3, 2, 1, 0, -1, -2, -3] # 0
+# puts volume [0, 1, 0, 1, 0] # 1
+# puts volume [0, 1, 0, 2, 0, 3, 1, 2, 1, 3] # 8
+# puts volume [0, 1, 0, 2, 0, 3, 1, 2, 1, 3, 0, 0] # 8
+# puts volume [0, 0, 0, 1, 0, 5, 0, 3, 0, 0, 6, 0, 0, 4, 0, 0] # 26
+
